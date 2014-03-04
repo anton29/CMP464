eb0a022d1d4672041f9b01dfd6696bd77a92143c
anton29 <abr8892@yahoo.com>
2014-03-04 14:35:11 -0500

    update

--- Hw3/src/com/example/hw3/SettingsActivity.java
+++ Hw3/src/com/example/hw3/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.example.hw3;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class SettingsActivity extends PreferenceActivity{
 	
@@ -9,6 +11,23 @@ public class SettingsActivity extends PreferenceActivity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
-	}
+		
+		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		if (settings.getBoolean("alt_1", true)) {
+		getApplication().setTheme(R.style.alt);
+		}else{
+		getApplication().setTheme(R.style.start);
+		}
+		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+		SharedPreferences settings1 = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		if (settings.getBoolean("alt_2", false)) {
+		getApplication().setTheme(R.style.alt);
+		}else{
+		getApplication().setTheme(R.style.start);
 
+	}
+	}
 }
